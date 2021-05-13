2d26863f2355d3357fccc8ee2b249ab852bd2a43
Aryakumar Jayanand Jha <aryakumarjha14@gmail.com>
2021-05-13 18:59:56 +0530

    Update detailScreen.dart

--- lib/views/detailScreen.dart
+++ lib/views/detailScreen.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
 
 class MovieDetail extends StatefulWidget {
-  MovieDetail(int id);
-
+  final int id;
+  MovieDetail(this.id);
   @override
   _MovieDetailState createState() => _MovieDetailState();
 }
 
 class _MovieDetailState extends State<MovieDetail> {
+  Movie movie;
+  var movieDetailRepo=MovieDetailRepository();
+  @override
+  void initState(){
+    movie=movieDetailRepo.fetchMovieDetails(widget.id);
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Text(""),
+        child: Text(movie.name),
       ),
     );
   }
